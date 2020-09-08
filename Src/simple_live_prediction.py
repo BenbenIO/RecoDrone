1ed4cc4fcddc05dcb6cb36996a627a5fac2300ca
benjamin <ioller.benjamin@gmail.com>
2020-09-08 19:02:52 +0900

    change feature generation, simple_live_inference script

--- Src/simple_live_prediction.py
+++ Src/simple_live_prediction.py
@@ -3,14 +3,16 @@ import pyaudio
 import wave
 import librosa
 import numpy as np
+import tflite_runtime.interpreter as tflite
 import time
+import argparse
 
 def extract_feature(x, FFT_SIZE=256):
     # Extract simple feature (abs to get the magnetude)
     mfccs = librosa.stft(librosa.util.normalize(x), n_fft=FFT_SIZE, window='hamming')
     mfccs = np.abs(mfccs)
     feat = np.mean(mfccs.T,axis=0)
-    return(feat)
+    return(np.array([feat]))
 
 def find_index_usb_audio(audio, name="BUFFALO"):
     for dev_i in range(audio.get_device_count()):
@@ -24,6 +26,18 @@ def find_index_usb_audio(audio, name="BUFFALO"):
             return(-1)
 
 def main():
+    ## Model file
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--model', help="Model file (.tflite)", dest='model_file', required=True)
+    args = parser.parse_args()
+    print("Loading {}.".format(args.model_file))
+
+    interpreter = tflite.Interpreter(model_path=args.model_file)
+    interpreter.allocate_tensors()
+    input_details = interpreter.get_input_details()
+    input_shape = input_details[0]['shape']
+    output_details = interpreter.get_output_details()
+
     ## Get Audio Stream
     audio_interface = pyaudio.PyAudio()
     usb_audio_i = find_index_usb_audio(audio_interface)
@@ -51,13 +65,23 @@ def main():
 
             # Pause the audio stream
             audio_stream.stop_stream()
-            print("Data: {}".format(data.shape))
+            print("\nData: {}".format(data.shape))
             start = time.time()
 
+            # Extract feature:
             feature = extract_feature(data)
 
+            ## TODO add feature.shape == input_shape check as test.
+
+            interpreter.set_tensor(input_details[0]['index'], feature)
+            interpreter.invoke()
+            output_data = interpreter.get_tensor(output_details[0]['index'])
+
             elapsed_time = time.time() - start
-            print("Feature {} extracted in {}".format(feature.shape, elapsed_time))
+            print("Feature ~ Inference in {} ms.".format(elapsed_time*1000))
+            print("Output data: {}".format(output_data))
+
+            ## TODO postprocessing from the output.
 
             # Resume the audio stream
             audio_stream.start_stream()
